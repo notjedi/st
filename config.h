6c23e0074c4154c807d8367c415c23fbb7e4a8de
jedi2610 <krithickumarub@protonmail.com>
2021-05-28 17:28:29 +0530

    add dracula theme patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
