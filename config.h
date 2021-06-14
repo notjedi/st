906e4cbce51cac3472b7557a838f44da2e75e5fc
jedi2610 <krithickumarub@protonmail.com>
2021-06-14 23:46:08 +0530

    change font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrains Mono Nerd Font:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "Noto Color Emoji:pixelsize=12:antialias=true:autohint=true" };
+static char *font2[] = { "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
